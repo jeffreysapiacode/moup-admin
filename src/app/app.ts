66a01bb4e90898c279d9a28f82b7eb08e6cfd645
jeffreysapia <jeffsapia1988@gmail.com>
2026-05-16 11:34:29 -0400

    Adding bootstrap and main component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Main } from './main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Main],
   templateUrl: './app.html',
-  styleUrl: './app.sass'
+  styleUrl: './app.sass',
 })
 export class App {
   protected readonly title = signal('moup-admin');
