f4c8ff910766d5db45be2b39d1813cebc6d17cf1
jeffreysapia <jeffsapia1988@gmail.com>
2026-07-05 19:17:41 -0400

    Added view container

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavRack } from './nav-rack/nav-rack';
+import { ViewContainer } from './view-container/view-container';
 
 @Component({
   selector: 'app-main',
-  imports: [NavRack],
+  imports: [NavRack, ViewContainer],
   templateUrl: './main.html',
   styleUrl: './main.sass',
 })
