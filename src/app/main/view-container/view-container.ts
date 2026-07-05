e786e7a0eab01184539117c61b303f68374d3e4d
jeffreysapia <jeffsapia1988@gmail.com>
2026-07-05 18:34:56 -0400

    Adding view container

---
+++ src/app/main/view-container/view-container.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-container',
+  imports: [],
+  templateUrl: './view-container.html',
+  styleUrl: './view-container.sass',
+})
+export class ViewContainer {}
