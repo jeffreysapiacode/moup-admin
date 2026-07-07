37d93bc8d9d753219f6f2dacdac21473ab558c31
jeffreysapia <jeffsapia1988@gmail.com>
2026-07-07 18:40:33 -0400

    Adding text area for description

--- src/app/main/view-container/content-view/upload/upload.ts
+++ src/app/main/view-container/content-view/upload/upload.ts
@@ -1,17 +1,18 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { FormGroup, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './upload.html',
   styleUrl: './upload.sass',
 })
 export class Upload {
-
   @Output() createContentOpen: EventEmitter<boolean> = new EventEmitter();
+  title?: string;
+  description?: string;
 
   close() {
     this.createContentOpen.emit(false);
   }
-
 }
