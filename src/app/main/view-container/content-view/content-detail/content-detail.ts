680d807d89e026341537b785646a4b0f24338d96
jeffreysapia <jeffsapia1988@gmail.com>
2026-07-17 04:18:06 -0400

    Adding formatting to numbers

--- src/app/main/view-container/content-view/content-detail/content-detail.ts
+++ src/app/main/view-container/content-view/content-detail/content-detail.ts
@@ -13,4 +13,18 @@ export class ContentDetail {
   close() {
     this.detailContentOpen.emit(false);
   }
+
+  formatNumber(value: string) {
+    let str = value.toString();
+    let count = 0;
+    let newStr = '';
+    for (let i = (str.length - 1); i >= 0; i--) {
+      if (count % 3 === 0 && count > 0) {
+        newStr = ',' + newStr;
+      }
+      newStr = str[i] + newStr;
+      count++;
+    }
+    return newStr;
+  }
 }
